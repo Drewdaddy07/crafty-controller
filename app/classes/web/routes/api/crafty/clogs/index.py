92dfd18dbd923cdff7eb456e70cc8bab745ceed3
amcmanu3 <mcdeweykp@gmail.com>
2024-04-20 15:47:34 -0400

    Refactor API for new activity log format

--- app/classes/web/routes/api/crafty/clogs/index.py
+++ app/classes/web/routes/api/crafty/clogs/index.py
@@ -1,3 +1,5 @@
+import os
+import json
 from app.classes.web.base_api_handler import BaseApiHandler
 
 
@@ -22,9 +24,17 @@ class ApiCraftyLogIndexHandler(BaseApiHandler):
             raise NotImplementedError
 
         if log_type == "audit":
+            with open(
+                os.path.join(self.controller.project_root, "logs", "audit.log"),
+                "r",
+                encoding="utf-8",
+            ) as f:
+                log_lines = [json.loads(line) for line in f]
+                rev_log_lines = log_lines[::-1]
+
             return self.finish_json(
                 200,
-                {"status": "ok", "data": self.controller.management.get_activity_log()},
+                {"status": "ok", "data": rev_log_lines},
             )
 
         if log_type == "session":
