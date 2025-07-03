f92a337886bf7e21e78704e1d8e3ba005a0b26b7
Andrew <mcdeweykp@gmail.com>
2025-07-03 17:32:35 -0400

    Create API handler for support package downloads

---
+++ app/classes/web/routes/api/crafty/clogs/support.py
@@ -0,0 +1,39 @@
+import os
+import json
+from pathlib import Path
+from app.classes.web.base_api_handler import BaseApiHandler
+
+
+class ApiCraftySupportIndexHandler(BaseApiHandler):
+    async def get(self):
+        auth_data = self.authenticate_user()
+        if not auth_data:
+            return
+        (
+            _,
+            _,
+            _,
+            _,
+            _,
+            _,
+        ) = auth_data
+
+        # All server permission checking occurs in the package function
+        await self.controller.package_support_logs(auth_data[4])
+
+        await self.download_file(
+            Path(
+                self.controller.project_root,
+                "temp",
+                str(auth_data[4]["user_id"]),
+                "support_logs",
+            ).with_suffix(".zip")
+        )
+        os.remove(
+            Path(
+                self.controller.project_root,
+                "temp",
+                str(auth_data[4]["user_id"]),
+                "support_logs",
+            ).with_suffix(".zip")
+        )
