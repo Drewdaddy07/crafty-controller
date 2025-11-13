e8892975a02c2235d3c84233f3ab7ab856703cbc
= <mcdeweykp@gmail.com>
2025-11-13 18:18:44 -0500

    Sonar

--- app/frontend/static/assets/js/shared/upload.js
+++ app/frontend/static/assets/js/shared/upload.js
@@ -201,8 +201,8 @@ function removeProgressItem(item_id) {
 }
 
 function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        const r = Math.random() * 16 | 0,
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replaceAll(/[xy]/g, function (c) {
+        const r = Math.trunc(Math.random() * 16),
             v = c === 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
