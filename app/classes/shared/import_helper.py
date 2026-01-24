2405317a2924ac9a9b3391a020dd5e86cb93db66
= <mcdeweykp@gmail.com>
2026-01-23 19:23:19 -0500

    Set executable perms

--- app/classes/shared/import_helper.py
+++ app/classes/shared/import_helper.py
@@ -194,10 +194,7 @@ class ImportHelpers:
             self.file_helper.ssl_get_file(
                 hytale_json.linux_installer_url, server_path, unix_exe
             )
-            st = os.stat(Path(server_path, unix_exe))
-            os.chmod(
-                Path(server_path, unix_exe), st.st_mode | stat.S_IEXEC
-            )  # set executable permissions
+            os.chmod(Path(server_path, unix_exe), 0o2760)  # set executable permissions
         process = subprocess.Popen(
             install_command,
             cwd=server_path,
